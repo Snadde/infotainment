69e741e71c1b0e2a273246599c8f1b5d3a380f64
Daniel Kvist <daniel@danielkvist.net>
2013-04-05 11:23:00 +0200

    #11 Created basic stage functionality

---
+++ apps/client/src/se/chalmers/pd/client/HostedApplicationController.java
@@ -0,0 +1,104 @@
+package se.chalmers.pd.client;
+
+import java.io.File;
+
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class HostedApplicationController {
+	
+	public interface Callbacks {
+		public void onLoadComplete(String url);
+	}
+
+	private final String DEFAULT_URL = "file:///android_asset/index.html";
+	private final String BASEDIR = Environment.getExternalStorageDirectory() + "/infotainment/apps/";
+	private WebView webView;
+	private Context context;
+	private Callbacks callback;
+
+	public HostedApplicationController(WebView webView, Context context) {
+		this.webView = webView;
+		this.context = context;
+		this.callback = (Callbacks) context;
+		
+		WebSettings webSettings = webView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		webSettings.setDomStorageEnabled(true);
+		webView.setWebViewClient(new CustomWebViewClient());
+		webView.setWebChromeClient(new CustomWebChromeClient());
+		webView.loadUrl(DEFAULT_URL);
+	}
+
+	public boolean init(String appName) {
+		File directory = new File(BASEDIR + appName);
+		if(directory.exists() && directory.isDirectory()) {
+			Log.d("HostedApplicationController", "init " + appName + " exists");
+			return true;
+		}
+		Log.d("HostedApplicationController",  "init " + appName + " doesn't exist or is not a directory");
+		return false;
+	}
+
+	public boolean install() {
+		String zipFilename = "webapp.zip";
+		String unzipLocation = BASEDIR;
+		Decompresser decompresser = new Decompresser(zipFilename, unzipLocation, context);
+		if(decompresser.unzip()) {
+			return true;
+		}
+		return false;
+	}
+
+	public void start(String location) {
+		webView.loadUrl("javascript:loadApp('" + "file://" + BASEDIR + location + "')");
+	}
+
+	public void stop() {
+		webView.loadUrl(DEFAULT_URL);
+	}
+
+	public void uninstall(String appName) {
+		Log.d("HostedApplicationController",  "uninstall " + appName);
+		File directory = new File(BASEDIR + appName);
+		if(directory.exists() && directory.isDirectory()) {
+			deleteRecursive(directory);
+		}
+	}
+	
+	private void deleteRecursive(File appDir) {
+		if (appDir.isDirectory()) {
+			for (File child : appDir.listFiles()) {
+				deleteRecursive(child);
+			}
+		}
+		appDir.delete();
+	}
+	
+	private class CustomWebChromeClient extends WebChromeClient {
+		public boolean onConsoleMessage(ConsoleMessage cm) {
+			Log.d("CustomWebChromeClient", cm.message() + " -- From line " + cm.lineNumber() + " of " + cm.sourceId());
+			return true;
+		}
+	}
+
+	private class CustomWebViewClient extends WebViewClient {
+
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			return false;
+		}
+
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			callback.onLoadComplete(url);
+			Log.d("CustomWebViewClient", "onPageFinished " + url);
+		}
+	}
+}
--- apps/client/src/se/chalmers/pd/client/MainActivity.java
+++ apps/client/src/se/chalmers/pd/client/MainActivity.java
@@ -2,56 +2,36 @@ package se.chalmers.pd.client;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.Window;
 import android.view.WindowManager;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements HostedApplicationController.Callbacks {
 
-	private final String DEFAULT_URL = "file:///android_asset/index.html";
-	private final String BASEDIR = Environment.getExternalStorageDirectory() + "/infotainment/apps/";
-	private WebView myWebView;
+	private WebView webView;
+	private HostedApplicationController controller;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main);
 		setupView();
-		unzip();
-	}
-
-	private void unzip() {
-		String zipFilename = "webapp.zip"; 
-		String unzipLocation = BASEDIR; 
-
-		DecompressZip d = new DecompressZip(zipFilename, unzipLocation, this);
-		
-		if(d.unzip()) {
-			myWebView.loadUrl("file://" + BASEDIR + "webapp/index.html");
-		} else {
-			myWebView.loadUrl(DEFAULT_URL);
-		}
 	}
 
 	private void setupView() {
-		myWebView = (WebView) findViewById(R.id.webview);
-		myWebView.setWebViewClient(new MyWebViewClient());
-		WebSettings webSettings = myWebView.getSettings();
-		webSettings.setJavaScriptEnabled(true);
+		webView = (WebView) findViewById(R.id.webview);
+		controller = new HostedApplicationController(webView, this);
 	}
-	
-	private class MyWebViewClient extends WebViewClient {
-	    @Override
-	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-	        return false;
-	    }
+
+	@Override
+	public void onLoadComplete(String url) {
+		if(controller.init("webapp")) {
+			//controller.start("/webapp/index.html");
+			controller.uninstall("webapp");
+		} else {
+			//controller.install();
+		}
 	}
 }
-
-
