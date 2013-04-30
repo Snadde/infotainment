c34764e7e34037f59deff7cafd9c3015b0d107f1
Daniel Kvist <daniel@danielkvist.net>
2013-04-30 16:45:32 +0200

    Added debug information and lowered minSdkVersion

--- apps/dashboard/src/se/chalmers/pd/dashboard/MainActivity.java
+++ apps/dashboard/src/se/chalmers/pd/dashboard/MainActivity.java
@@ -31,6 +31,11 @@ public class MainActivity extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+//		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+//        .detectAll()
+//        .penaltyLog()
+//        .build());
+//		
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
