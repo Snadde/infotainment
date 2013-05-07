7b90fea4c7bed9815d95077cee677bf1c4902345
Daniel Kvist <daniel@danielkvist.net>
2013-05-07 10:41:55 +0200

    #24 Playlist update on next, play, pause

--- apps/webapp/Playlist/javascripts/main.js
+++ apps/webapp/Playlist/javascripts/main.js
@@ -1,104 +1,5 @@
 $(document).ready(function() {
-    var COMMAND_ADD = "add"
-    var COMMAND_NEXT = "next";
-    var COMMAND_PLAY = "play";
-    var PRIVATE_CHANNEL = "/playlist";
-    var debug = true;
     
-    /*
-     *  Dev tests
-     */
-    
-    function testAddMessage() {
-        var testMessage = {
-            topic : PRIVATE_CHANNEL,
-            payload : {
-                action : 'add',
-                data : {
-                    artist : 'Nirvana',
-                    track : 'About a girl'
-                }
-            }
-        }
-        onMessage(JSON.stringify(testMessage));
-    }
-    
-    function testNextMessage() {
-        var testMessage = {
-            topic : PRIVATE_CHANNEL,
-            payload : {
-                action : 'next'
-            }
-        }
-        onMessage(JSON.stringify(testMessage));
-    }
-    
-    function testPlayMessage() {
-        var testMessage = {
-            topic : PRIVATE_CHANNEL,
-            payload : {
-                action : 'play'
-            }
-        }
-        onMessage(JSON.stringify(testMessage));
-    }
-    
-    /*
-    testAddMessage();
-    testAddMessage();
-    testPlayMessage();
-    */
-    
-    /*
-     *  Implementation
-     */
-    
-    subscribe(PRIVATE_CHANNEL);
-    
-    function subscribe(topic) {
-        WebApp.publish(topic);        
-    }
-    
-    function publish(topic, message) {
-        WebApp.publish(topic, message);
-    }
-
-	function onMessage(message) {
-		log("onMessage: " + message);
-        var json_message = JSON.parse(message);
-        if(json_message.topic == PRIVATE_CHANNEL) {
-            handleMessagePayload(json_message.payload);
-        }
-	}
-    
-    function handleMessagePayload(payload) {
-		log("handleMessagePayload: " + payload);
-        if(payload.action == COMMAND_ADD) {
-            updateList(payload.data);
-        } else if (payload.action == COMMAND_NEXT) {
-            $('#options-list li:first').remove();
-        } else if(payload.action = COMMAND_PLAY) {
-            $('#current-song-section h2').html(
-                $('#options-list li:first span.artist').text() + " &ndash; " +
-                $('#options-list li:first span.track').text()
-            );
-        }
-    }
-    
-    function updateList(data) {
-        $('#options-list').append(
-            $('<li/>', {
-                html: '<span class="artist">' + data.artist + '</span>' + "<br/>" + 
-                        '<span class="track">' + data.track + '</span>'
-            })
-        );
-    }
-
-	function log(message) {
-		if (debug) {
-			console.log(message);
-		}
-	}
     
     $('#search-form').submit(function() {
         $.spotifyTrackSearch($('#search-input').val(), function(data) {
@@ -114,3 +15,111 @@ $(document).ready(function() {
         return false;
     });
 });
+
+ var COMMAND_ADD = "add"
+ var COMMAND_NEXT = "next";
+ var COMMAND_PLAY = "play";
+ var COMMAND_PAUSE = "pause";
+ var PRIVATE_CHANNEL = "/playlist";
+ var debug = true;
+ 
+ /*
+  *  Dev tests
+  */
+ 
+ function testAddMessage() {
+     var testMessage = {
+         action : 'add',
+         artist : 'Nirvana',
+         track : 'About a girl'
+     }
+     onMessage("/playlist", testMessage);
+ }
+ 
+ function testNextMessage() {
+     var testMessage = {
+         action : 'next'
+     }
+     onMessage("/playlist", testMessage);
+ }
+ 
+ function testPlayMessage() {
+     var testMessage = {
+         action : 'play'
+     }
+     onMessage("/playlist", testMessage);
+ }
+ 
+ function testPauseMessage() {
+     var testMessage = {
+         action : 'pause'
+     }
+     onMessage("/playlist", testMessage);
+ }
+ 
+ 
+ /*
+  *  Implementation
+  */
+ 
+ subscribe(PRIVATE_CHANNEL);
+ 
+ function subscribe(topic) {
+     WebApp.subscribe(topic);        
+ }
+ 
+/* function publish(topic, message) {
+     WebApp.publish(topic, message);
+ }*/
+
+function onMessage(topic, payload) {
+	log("onMessage: " + topic + " payload " + JSON.stringify(payload));
+     if(topic == PRIVATE_CHANNEL) {
+         handleMessagePayload(payload);
+     }
+}
+ 
+ function handleMessagePayload(payload) {
+	log("handleMessagePayload: " + payload);
+     if(payload.action == COMMAND_ADD) {
+         updateList(payload);
+     } else if (payload.action == COMMAND_NEXT) {
+         $('#options-list li:first').slideUp(function() {
+             $(this).parent('ul').append($(this)).find('li:last').fadeIn();
+             updatePlayingInfo();
+         });
+     } else if(payload.action = COMMAND_PLAY) {
+         updatePlayingInfo();
+         toggleButtons();
+     } else if(payload.action = COMMAND_PAUSE) {
+         playing = false;
+         toggleButtons();
+     }
+ }
+ 
+ function updatePlayingInfo() {
+     $('#current-song-section h2').html(
+         $('#options-list li:first span.artist').text() + " &ndash; " +
+         $('#options-list li:first span.track').text()
+     );
+ }
+ 
+ function toggleButtons() {
+     $('#play').toggle();
+     $('#pause').toggle();
+ }
+ 
+ function updateList(payload) {
+     $('#options-list').append(
+         $('<li/>', {
+             html: '<span class="artist">' + payload.artist + '</span>' + "<br/>" + 
+                     '<span class="track">' + payload.track + '</span>'
+         }).hide().fadeIn()
+     );
+ }
+
+function log(message) {
+	if (debug) {
+		console.log(message);
+	}
+}
