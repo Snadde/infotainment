9361add5b7c33b9b5569d30af461c77cd7272dd2
Daniel Kvist <daniel@danielkvist.net>
2013-05-09 11:56:41 +0200

    #27 documented classes and methods

--- apps/sensors/src/se/chalmers/pd/sensors/Main.java
+++ apps/sensors/src/se/chalmers/pd/sensors/Main.java
@@ -1,4 +1,5 @@
 package se.chalmers.pd.sensors;
+
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,23 +8,34 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
+/**
+ * This is the main class of the sensor application. It has three buttons to
+ * trigger play, next and pause actions. This class contains the basic GUI
+ * constructs such as the frame and buttons itself. It also has an action
+ * listener that tells the application controller to perform an action when a
+ * button is clicked.
+ * 
+ */
 public class Main implements ActionListener {
 
 	private static final String SENSOR_PLAY = "Play sensor";
 	private static final String SENSOR_NEXT = "Next sensor";
 	private static final String SENSOR_PAUSE = "Pause sensor";
-	
+
 	private static final String ACTION_PLAY = "play";
 	private static final String ACTION_NEXT = "next";
 	private static final String ACTION_PAUSE = "pause";
-	
+
 	private JButton sensor1;
 	private JButton sensor2;
 	private JButton sensor3;
 	private JFrame jframe;
 	private Controller controller;
-	
+
+	/**
+	 * Initiates the application and creates the basic components and adds the
+	 * listeners.
+	 */
 	public Main() {
 		createComponents();
 		addComponents();
@@ -31,7 +43,10 @@ public class Main implements ActionListener {
 		controller = new Controller();
 		jframe.setVisible(true);
 	}
-	
+
+	/**
+	 * Creates the specific components of the GUI
+	 */
 	private void createComponents() {
 		jframe = new JFrame("Sensors");
 		jframe.setSize(600, 400);
@@ -42,42 +57,52 @@ public class Main implements ActionListener {
 		sensor3 = new JButton(SENSOR_PAUSE);
 		sensor3.setActionCommand(ACTION_PAUSE);
 	}
-	
+
+	/**
+	 * Adds the components to a panel and jframe
+	 */
 	private void addComponents() {
 		JPanel panel = new JPanel();
-		GridLayout grid = new GridLayout(3,2);
+		GridLayout grid = new GridLayout(3, 2);
 		panel.setLayout(grid);
 		panel.add(sensor1);
 		panel.add(sensor2);
-		panel.add(sensor3);		
-		
+		panel.add(sensor3);
+
 		jframe.getContentPane().add(panel);
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
+	/**
+	 * Adds this class as listener to the buttons
+	 */
 	private void addListeners() {
 		sensor1.addActionListener(this);
 		sensor2.addActionListener(this);
 		sensor3.addActionListener(this);
 	}
-	
+
+	/**
+	 * Executed when a button is clicked and tells the controller to perform the
+	 * specific action that the button that was clicked represents.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String command = e.getActionCommand();
-		if(command.equals(ACTION_PLAY)) {
+		if (command.equals(ACTION_PLAY)) {
 			controller.play();
-		} else if(command.equals(ACTION_NEXT)) {
+		} else if (command.equals(ACTION_NEXT)) {
 			controller.next();
-		} else if(command.equals(ACTION_PAUSE)) {
+		} else if (command.equals(ACTION_PAUSE)) {
 			controller.pause();
 		}
 	}
-	
+
 	/**
-	 * @param args
+	 * Instantiates the application
 	 */
 	public static void main(String[] args) {
 		new Main();
 	}
-	
+
 }
--- apps/sensors/src/se/chalmers/pd/sensors/MqttWorker.java
+++ apps/sensors/src/se/chalmers/pd/sensors/MqttWorker.java
@@ -6,11 +6,14 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
 
-
 /**
  * This worker launches an MQTT client in a separate thread and subscribes to
  * the systems basic topics. When a message is received, it broadcasts the
  * message using custom intent filters.
+ * 
+ * When the thread is started, the first thing the worker does is to create a
+ * directory which it can use for temporary storage of mqtt messages. It then
+ * connects to the broker.
  * 
  */
 public class MqttWorker extends Thread {
@@ -20,16 +23,15 @@ public class MqttWorker extends Thread {
 	private static final String BROKER = "tcp://192.168.43.147:1883";
 	private static final String CLIENT_NAME = "sensors";
 	private MqttClient mqttClient;
-	
+
 	public MqttWorker() {
-		
+
 	}
 
 	@Override
 	public void run() {
 		try {
 			this.setName(WORKER_NAME);
-			// Sets up the client and subscribes to topics
 			String tmpDir = System.getProperty("user.dir") + STORAGE_DIRECTORY;
 			MqttDefaultFilePersistence dataStore = new MqttDefaultFilePersistence(tmpDir);
 			mqttClient = new MqttClient(BROKER, CLIENT_NAME, dataStore);
@@ -38,11 +40,12 @@ public class MqttWorker extends Thread {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Publishes a message on the given topic.
 	 * 
 	 * @param topic
+	 *            the topic to publish on
 	 * @param message
 	 *            should be stringified JSON
 	 */
@@ -57,8 +60,16 @@ public class MqttWorker extends Thread {
 			e.printStackTrace();
 		}
 	}
-	
-	private void log(String topic, String message) {
-		System.out.println(topic + ", " + message);
+
+	/**
+	 * Helper method that logs to system out.
+	 * 
+	 * @param title
+	 *            a title reference to the output
+	 * @param message
+	 *            a message containing debug information
+	 */
+	private void log(String title, String message) {
+		System.out.println(title + ", " + message);
 	}
 }
