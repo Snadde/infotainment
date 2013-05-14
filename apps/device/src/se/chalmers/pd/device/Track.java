ddb91cc8959b9f332246ce6ed1259e3aae88a4ee
Snadde <thituson@gmail.com>
2013-05-14 16:16:23 +0200

    Fix #45 updated track class with length of track

--- apps/device/src/se/chalmers/pd/device/Track.java
+++ apps/device/src/se/chalmers/pd/device/Track.java
@@ -1,16 +1,28 @@
 package se.chalmers.pd.device;
 
-public class Track {
-	
-	
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Track implements Parcelable {
 	private String name;
 	private String artist;
 	private String spotifyUri;
+	private int length;
 
-	public Track(String name, String artist, String spotifyUri) {
+	public Track() { }
+
+	public Track(String name, String artist, String spotifyUri, int length) {
 		this.name = name;
 		this.artist = artist;
 		this.spotifyUri = spotifyUri;
+		this.setLength(length);
+	}
+
+	public Track(Parcel parcel) {
+		this.name = parcel.readString();
+		this.artist = parcel.readString();
+		this.spotifyUri = parcel.readString();
+		this.setLength(parcel.readInt());
 	}
 
 	/**
@@ -33,15 +45,81 @@ public class Track {
 	public String getUri() {
 		return spotifyUri;
 	}
-	
+
+	/**
+	 * @param the
+	 *            name of the track
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @param the
+	 *            name of the artist
+	 */
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	/**
+	 * @param the
+	 *            spotify URI
+	 */
+	public void setUri(String spotifyUri) {
+		this.spotifyUri = spotifyUri;
+	}
+
 	@Override
-	public boolean equals(Object object){
+	public boolean equals(Object object) {
 		if (object instanceof Track) {
 			Track track = (Track) object;
-			if(track.name.equals(this.name)&&track.artist.equals(this.artist)&&track.spotifyUri.equals(this.spotifyUri))
+			if (track.name.equals(this.name) && track.artist.equals(this.artist)
+					&& track.spotifyUri.equals(this.spotifyUri))
 				return true;
 		}
 		return false;
 	}
+
+	public int describeContents() {
+		return 0;
+	}
+
+	/**
+     * Writes the data to the parcel
+     */
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(name);
+		dest.writeString(artist);
+		dest.writeString(spotifyUri);
+		dest.writeInt(length);
+	}
+
+	/**
+	 * @return the length
+	 */
+	public int getLength() {
+		return length;
+	}
+
+	/**
+	 * @param length the length to set
+	 */
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	/**
+	 * Parcelable construction
+	 */
+	public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+		public Track createFromParcel(Parcel in) {
+			return new Track(in);
+		}
+
+		public Track[] newArray(int size) {
+			return new Track[size];
+		}
+	};
 
 }
